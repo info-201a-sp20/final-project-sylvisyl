b16b80f0b44e2ce7730ed7c64c6da9230c73e581
sylvia sun <sunz35@uw.edu>
2020-05-17 22:06:15 -0700

    finished first visualization and added it into the index file with description and fixed style

--- scripts/Aggregate Table script.R
+++ scripts/Aggregate Table script.R
@@ -11,8 +11,10 @@ kc_housing <- read.csv("data/kc_house_data.csv", stringsAsFactors = FALSE)
 summary_table <- function(dataset) {
   dataset %>%
     group_by(zipcode) %>%
-    select(zipcode, price, bedrooms, bathrooms, 
-           sqft_living, yr_built, condition) %>%
+    select(
+      zipcode, price, bedrooms, bathrooms,
+      sqft_living, yr_built, condition
+    ) %>%
     summarize(
       avg_price = round(mean(price, na.rm = TRUE), 0),
       num_bedrooms = round(mean(bedrooms, na.rm = TRUE), 0),
@@ -20,10 +22,10 @@ summary_table <- function(dataset) {
       avg_sqft_living = round(mean(sqft_living, na.rm = TRUE), 1),
       avg_yr_built = round(mean(yr_built, na.rm = TRUE), 0),
       avg_condition = round(mean(condition, na.rm = TRUE), 1)
-    ) 
+    )
 }
 
-# Important information sectiob
+# Important information section
 
 # Finding the zipcode that has the highest sale price
 most_expensive_zip <- summary_table(kc_housing) %>%
@@ -37,7 +39,6 @@ most_expensive_price <- summary_table(kc_housing) %>%
   pull(avg_price)
 
 # Finding the zipcode that has the oldest houses in average
-
 oldest_zipcode <- summary_table(kc_housing) %>%
   select(zipcode, avg_yr_built) %>%
   filter(avg_yr_built == min(avg_yr_built, na.rm = TRUE)) %>%
