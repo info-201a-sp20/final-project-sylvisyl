7eb5b1c050dfd23d48e3644bd1128403f4a93f8a
cyj1027837318 <ycheng56@uw.edu>
2020-05-17 22:52:58 -0700

    finish avg_price vs yr_built plot

---
+++ scripts/visual_3.R
@@ -0,0 +1,22 @@
+# Set up
+library(dplyr)
+library(lintr)
+library(styler)
+library(ggplot2)
+library(plotly)
+
+kc_housing <- read.csv("data/kc_house_data.csv", stringsAsFactors = FALSE)
+
+
+#grouping and filtering datasets
+#2variables: year_built vs avg_price
+#plot type : line plot
+#purpose: indicate the trend of price as built years vary
+year_built_chart <- kc_housing %>%
+   group_by(yr_built) %>%
+   summarise(avg_price = mean(price))
+
+year_build_plot <- ggplot(year_built_chart,aes(x=yr_built, y=avg_price)) +
+  geom_line()+
+  geom_point()
+ggplotly(year_build_plot)  
