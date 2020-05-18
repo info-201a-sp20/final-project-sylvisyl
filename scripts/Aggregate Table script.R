4a9d95bdd26a9292fd12cfd51e300fd937d8c27a
sylvia sun <sunz35@uw.edu>
2020-05-17 20:16:56 -0700

    created the index.rmd file and added summary table in it

--- scripts/Aggregate Table script.R
+++ scripts/Aggregate Table script.R
@@ -7,15 +7,18 @@ kc_housing <- read.csv("data/kc_house_data.csv", stringsAsFactors = FALSE)
 
 # Summary table
 
-summary_table <- kc_housing %>%
-  group_by(zipcode) %>%
-  select(zipcode, price, bedrooms, bathrooms, 
-         sqft_living, yr_built, condition) %>%
-  summarize(
-    avg_price = round(mean(price, na.rm = TRUE), 1),
-    avg_bedrooms = round(mean(bedrooms, na.rm = TRUE),0),
-    avg_bathrooms = round(mean(bathrooms, na.rm = TRUE), 0),
-    avg_sqft_living = round(mean(sqft_living, na.rm = TRUE), 1),
-    avg_yr_built = round(mean(yr_built, na.rm = TRUE), 0),
-    avg_condition = round(mean(condition, na.rm = TRUE), 0)
-  ) 
+summary_table <- function(dataset) {
+  dataset %>%
+    group_by(zipcode) %>%
+    select(zipcode, price, bedrooms, bathrooms, 
+           sqft_living, yr_built, condition) %>%
+    summarize(
+      avg_price = round(mean(price, na.rm = TRUE), 1),
+      avg_bedrooms = round(mean(bedrooms, na.rm = TRUE),0),
+      avg_bathrooms = round(mean(bathrooms, na.rm = TRUE), 0),
+      avg_sqft_living = round(mean(sqft_living, na.rm = TRUE), 1),
+      avg_yr_built = round(mean(yr_built, na.rm = TRUE), 0),
+      avg_condition = round(mean(condition, na.rm = TRUE), 0)
+    ) 
+}
+
