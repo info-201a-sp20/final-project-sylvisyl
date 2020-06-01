74f2bea13a301362fbf556bbfd19fd874a93cf5a
cyj1027837318 <ycheng56@uw.edu>
2020-05-31 22:34:57 -0700

    crime map intereactive page done

---
+++ scripts/crime_map.R
@@ -0,0 +1,113 @@
+#setup
+
+library(dplyr)
+library(leaflet)
+library(shiny)
+kc_housing <- read.csv("../data/kc_house_data.csv",
+                       stringsAsFactors = FALSE)
+crime <- read.csv("../data/SPD_Officer_Involved_Shooting__OIS__Data.csv",
+                  stringsAsFactors = FALSE)
+crime <- crime %>%
+  filter(State == "WA")
+
+#widget 1 : sliderbar for zipcode
+widget_1 <- sliderInput(
+  "zc",
+  "1. Select the range of zipcode:",
+  value = c(98101, 98199),
+  min = 98101,
+  max = 98199
+)
+
+#widget 2: sliderbar for price
+widget_2 <- sliderInput(
+  "prices",
+  "2. Select the budget:",
+  value = c(75000, 7700000),
+  min = 75000,
+  max = 7700000
+)
+
+#widget 3: checkbox group for crime status
+widget_3 <- selectInput("case", "3.Select the cases shown:",
+                               c("serve cases" = "Yes",
+                                 "normal cases" = "No")
+)
+#layout
+map_page <- tabPanel(
+  "Crime and housing",
+  titlePanel("King couty housing and security"),
+  sidebarLayout(
+    sidebarPanel(
+      widget_1,
+      br(),
+      widget_2,
+      br(),
+      widget_3
+    ),
+    mainPanel(
+      leafletOutput("crime_map")
+    )
+  )
+)
+
+ui <- fluidPage(
+  mainPanel(
+    map_page
+  )
+)
+
+#server
+server <- function(input, output) {
+  crime_filtered <- reactive({
+    crime %>%
+      filter(Officer.Injured == input$case) %>%
+      select(Date,
+             Longitude,
+             Latitude,
+             Officer.Injured)
+  })
+  kc_filtered <- reactive({
+    kc_housing %>%
+      filter(zipcode >= input$zc[1]) %>%
+      filter(zipcode <= input$zc[2]) %>%
+      filter(price >= input$prices[1]) %>%
+      filter(price <= input$prices[2]) %>%
+      select(price,
+             bedrooms,
+             bathrooms,
+             zipcode,
+             lat,
+             long)
+  })
+  output$crime_map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addCircleMarkers(
+        data = crime_filtered(),
+        lng = ~Longitude,
+        lat = ~Latitude,
+        color = "red",
+        popup = paste0(crime_filtered()$Longtitude,
+                       "\n",
+                       crime_filtered()$Latitude)
+      ) %>%
+      addCircleMarkers(
+        data = kc_filtered(),
+        lng = ~long,
+        lat = ~lat,
+        color = "blue",
+        popup = paste0("price:",
+                       kc_filtered()$price,
+                       "\n",
+                       "bedroom#:",
+                       kc_filtered()$bedrooms,
+                       "\n",
+                       "bathroom#:",
+                       kc_filtered()$bathrooms,
+                       "\n",
+                       "zipcode:",
+                       kc_filtered()$zipcode)
+      )
+  })
+}
--- scripts/summary_takeaway.R
+++ scripts/summary_takeaway.R
@@ -7,7 +7,25 @@ library(lintr)
 library(styler)
 
 kc_housing <- read.csv("../data/kc_house_data.csv", stringsAsFactors = FALSE)
-
+kc_crime <- kc_housing %>%
+  filter(price >= 25000)
+crime <- read.csv("../data/SPD_Officer_Involved_Shooting__OIS__Data.csv",
+                  stringsAsFactors = FALSE)
+crime_filtered_summary <- crime %>%
+  filter(State == "WA") %>%
+  filter(Officer.Injured == "No")
+summary_crime_map <- leaflet() %>%
+  addCircleMarkers(
+    data = crime_filtered_summary,
+    lng = ~Longitude,
+    lat = ~Latitude,
+    color = "red"
+  ) %>%
+  addCircleMarkers(
+    data = kc_crime(),
+    lng = ~long,
+    lat = ~lat,
+    color = "blue")
 
 summary_page <- tabPanel(
   "Conclusion",
@@ -28,6 +46,23 @@ summary_page <- tabPanel(
     three to four people per household. King county people
     feel comfortable living and are able to afford a house
     that is less than 3000 sqft living space."),
-  tableOutput(outputId = "table")
+  tableOutput(outputId = "table"),
+  br(),
+  h3("Second summary takeaway"),
+  p("Based on the crime rates intereact maps and the following attache
+  chart (blue = houses, and red = crime cases), we can notice that the
+    region that often happen crime cases tend to have lower selling prices,
+    compared with the high selling prices regions. The houses have higher prices
+    tend to locate in suburb. Many factors play the role in that correlations but
+    no doubuts that the security is one of the main one."),
+  h4("Implications:"),
+  p("By play with the graph and analyzing the correlation between crime rates and 
+    housing prices, we could conclude that safety has played a huge role in affecting
+    housing prices in king county. By using the map, we could predit : first, which 
+    region might need a higher proportion of armed gurads to stabilize the community; 
+    second, we could vaguely predict the prices of a house based on the region and 
+    the crime rates around it."),
+  leafletOutput(outputId = "summary_crime_map"),
+  br(),
   )
 )
